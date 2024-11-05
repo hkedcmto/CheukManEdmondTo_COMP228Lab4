611e8e6c4aeeb7625487df0121bc98ad1ec207bd
hkedcmto <edtca23@gmail.com>
2024-11-05 02:59:32 -0500

    1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
-module com.example.cheukmanedmondto_comp228lab4 {
+module exercise1 {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.example.cheukmanedmondto_comp228lab4 to javafx.fxml;
-    exports com.example.cheukmanedmondto_comp228lab4;
+    exports exercise1; // Exports the package containing the StudentInfo class
 }
